98c22d5fc006d28c9f46ba88e57600e194eda3f2
AlexanderSchwarzppedvAG <alexanders@ppedv.de>
2022-06-02 13:44:15 +0200

    samples

---
+++ new-SPSite.ps1
@@ -0,0 +1,12 @@
+﻿Add-PSSnapin *share*
+
+#get-command *spsite*
+
+#get-help New-SPSite -Detailed
+
+New-SPSite -Url http://hv-sharepoint1/websites/intranet-modern -Template sts#3 -Name Intranet-Modern -ContentDatabase wss_content -OwnerAlias sp-training\administrator
+
+#Get-SPWebTemplate
+
+New-SPSite -Url http://hv-sharepoint1/websites/POA -Template SITEPAGEPUBLISHING#0 -Name POA -ContentDatabase wss_content -OwnerAlias sp-training\administrator
+	
