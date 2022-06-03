4eeca54c15e30304dc63a52ee2acba50878b70d6
AlexanderSchwarzppedvAG <alexanders@ppedv.de>
2022-06-03 11:22:54 +0200

    samples2

---
+++ SammlungMitCustomTemplateErstellen.ps1
@@ -0,0 +1,115 @@
+﻿Add-PSSnapin Microsoft.Sharepoint.Powershell
+## 
+##### Sitecollection mit Custom Template in der Shell: Alexander Schwarz
+##### Websitesammlung anlegen
+##### wichtig!
+##### line13 Url eintragen
+##### line19 Languagecode und Account eintragen (1031=de-de ; 1033=en-us)       
+##### line32 Pfad zum Template eintragen
+##### line56/74 Templatename eintragen
+##### line87 Languagecode eintragen
+##### line112 Template-ID eintragen  
+
+ $mainurl = “http://shp16/sites/customtemplate1”
+
+
+         Write-Host -ForegroundColor White ” – Creating Site Collection  $mainurl…” -NoNewline
+
+
+         New-SPSite $mainurl -OwnerAlias sample\administrator  -Name “Mein Custom Template funtioniert” -Language 1031
+
+
+         Write-Host -ForegroundColor GREEN “Done.” 
+
+
+         Write-Host
+
+##### Template von einem Pfad in den Solutionkatalog hochladen bzw. installieren und aktivieren
+
+         Write-Host -ForegroundColor White ” – Adding and Installing Your Custom Template…” –NoNewline]
+
+
+              Add-SPUserSolution -LiteralPath C:\templates\testtemplate.wsp -Site $mainurl
+
+
+             $ErrorActionPreference = “silentlycontinue”
+
+
+                do
+
+
+                {
+
+
+Write-Host “.” -NoNewline -ForeGroundColor White;
+
+
+                                Start-Sleep -Seconds 5;                                 
+
+
+                                try
+
+
+                                {
+
+
+                                $testsolution = Get-SPUserSolution -Identity testtemplate.wsp -Site $mainurl
+
+
+                                }
+
+
+                                catch
+
+
+                                {}
+
+
+                } while(!$testsolution);
+
+
+          $ErrorActionPreference = “stop”
+
+
+          Install-SPUserSolution -Identity testtemplate.wsp -Site $mainurl
+
+
+          Write-Host -ForegroundColor GREEN “Done.” 
+
+
+           Write-Host   
+
+##### ID des Templates ausfindig machen          
+
+           $site= new-Object Microsoft.SharePoint.SPSite($mainurl )
+
+
+$loc= [System.Int32]::Parse(1031)
+
+
+$templates= $site.GetWebTemplates($loc)
+
+
+foreach ($child in $templates)
+
+
+{
+
+
+    write-host $child.Name ”  ” $child.Title
+
+
+}
+
+
+$site.Dispose()
+
+##### ID wie in line105 eintragen (die ID ist hardcoded, also auch übertragbar!)
+
+$web = GET-SPWeb $mainurl 
+
+
+$web.ApplyWebTemplate(“{32C8B557-4AB4-4FC7-A45D-67FE3F7645BA}#testtemplate”)
+
+
+
---
+++ kerberos.ps1
@@ -0,0 +1,22 @@
+﻿#für SQL-Server
+setspn -s MSSQLSvc/sp19sub1:1433 sample\sqlservice				
+setspn -s MSSQLSvc/sp19sub1.sample.de:1433 sample\sqlservice
+
+#für Webanwendung
+setspn -s HTTP/sp19sub1 sample\spservice 			
+setspn -s HTTP/sp19sub1.sample.de sample\spservice
+
+#für ZentralAdministration
+setspn -s HTTP/shp19-1:5331 sample\spfarm 				
+setspn -s HTTP/shp19-1.sample.de:5331 sample\spfarm
+
+#für MysiteHost
+#setspn -s HTTP/mysite svc-spmyapppool
+#setspn -s HTTP/mysite.sample.de svc-spmyapppool
+
+#In der ZA bei den Webanwendungen die Auth. auf "Kerberos" umstellen
+#Danach prüfen, ob im IIS die Auth bei Windowsauth.-> Anbieter auf "Negotiate" steht!
+#Mit klist prüfen ob Ticket ausgestellt wurde
+#Mit klist purge Tickets löschen
+
+#Eventlog IDs mit 4624 prüfen!
